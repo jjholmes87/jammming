642c13e5d6d38b069d3cc0ad164bc2d18d7ecb30
JHolmes87 <64210391+JHolmes87@users.noreply.github.com>
2024-07-02 17:01:24 +0700

    removed router import in index

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' //test
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-      </Switch>
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
