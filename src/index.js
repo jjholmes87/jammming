801b84009799bcc8fe3047830b38f5c9a1a4680a
JHolmes87 <64210391+JHolmes87@users.noreply.github.com>
2024-07-02 17:00:11 +0700

    two changes trying to fix incorrect routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' //test
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>
 );
 
